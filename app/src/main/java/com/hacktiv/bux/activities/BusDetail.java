c12ae6d2909981eb2dbe1865b8e6c062a497f497
fajar <fajar.wira1403@gmail.com>
2022-12-15 21:36:30 +0700

    Bus Schedule

---
+++ app/src/main/java/com/hacktiv/bux/activities/BusDetail.java
@@ -0,0 +1,16 @@
+package com.hacktiv.bux.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hacktiv.bux.R;
+
+public class BusDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_detail);
+    }
+}
